31a4d8a00a72887b8b034a4e4fa5decabd3fbbde
Mike Buchanan <michael.buchanan@coheretechnology.com>
2018-06-10 16:34:01 -0400

    slight change to data generation actions

--- config.py
+++ config.py
@@ -1,30 +1,46 @@
 import numpy as np
 import random
 
-def generate_data_action(t, current_action):
-#     a = env.action_space.sample()
-#     return a
-    if t < 60:
-        return np.array([0,1,0])
-    
-    if t % 5 > 0:
-        return current_action
 
-    rn = random.randint(0,9)
+# steer, gas, brake
+def generate_data_action(t, prev_action):
+    # action = env.action_space.sample()
+    # return action
+
+    # accelerate at beginning
+    if t < 50:
+        return np.array([0, 1, 0])
+
+    # stick with an action for a few time steps
+    if t % 5 > 0:
+        return prev_action
+
+    # choose a random discrete action
+    rn = random.randint(0, 9)
+    # Coast
     if rn in [0]:
-        return np.array([0,0,0])
-    if rn in [1,2,3,4]:
-        return np.array([0,random.random(),0])
-    if rn in [5,6,7]:
-        return np.array([-random.random(),0,0])
+        return np.array([0, 0, 0])
+    # Accelerate straight
+    if rn in [1, 2, 3, 4, 5]:
+        return np.array([0, random.random(), 0])
+    # Turn left
+    if rn in [6, 7]:
+        return np.array([-random.random(), 0, 0])
+    # Turn right
     if rn in [8]:
-        return np.array([random.random(),0,0])
+        return np.array([random.random(), 0, 0])
+    # Brake
     if rn in [9]:
-        return np.array([0,0,random.random()])
+        return np.array([0, 0, random.random()])
+
+
+def normalize_observation(observation):
+    return observation.astype('float32') / 255.
 
 
 def adjust_obs(obs):
-    return obs.astype('float32') / 255.
+    return normalize_observation(obs)
+
 
 train_envs = ['car_racing']
 test_envs = ['car_racing']
